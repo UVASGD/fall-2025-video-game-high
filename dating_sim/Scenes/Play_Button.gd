2d189f6f57be2b14ffcfaeed14f37827e1bc16d6
BloobErt <gtlothamer@gmail.com>
2025-12-06 01:29:53 -0500

    Added the credits scene yippee!!
    credits scene as second button on menu, added button that goes back to menu

--- dating_sim/Scenes/Play_Button.gd
+++ dating_sim/Scenes/Play_Button.gd
@@ -3,3 +3,7 @@ extends TextureButton
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Tutorial.tscn")
+
+
+func _on_texture_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Credits.tscn")
